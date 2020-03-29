f923f4a79537ff12548f677f89de95d9f91b09c3
Craig Livings <CraigLivings@gmail.com>
2020-03-29 14:47:12 +0100

    Added pictures

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,34 +8,35 @@ import { Component, HostListener } from "@angular/core";
 export class AppComponent {
   text = "Aa";
   name = "amber";
+  image = "amber.jpg";
 
   letters = {
-    a: { text: "amber" },
-    b: { text: "banana" },
-    C: { text: "cat" },
-    d: { text: "daddy" },
-    e: { text: "elephant" },
-    f: { text: "fox" },
-    g: { text: "giraffe" },
-    h: { text: "house" },
-    i: { text: "ice cream" },
-    j: { text: "jump" },
-    k: { text: "king" },
-    l: { text: "lion" },
-    m: { text: "mummy" },
+    a: { text: "amber", image: "amber.jpg" },
+    b: { text: "banana", image: "banana.jfif" },
+    c: { text: "cat", image: "cat.jpeg" },
+    d: { text: "daddy", image: "daddy.jpg" },
+    e: { text: "elephant", image: "elephant.jpg" },
+    f: { text: "fox", image: "fox.jpg" },
+    g: { text: "giraffe", image: "giraffe.jpg" },
+    h: { text: "house", image: "house.jpg" },
+    i: { text: "ice cream", image: "icecream.jpg" },
+    j: { text: "jug", image: "jug.jpg" },
+    k: { text: "kangaroo", image: "kangaroo.jpg" },
+    l: { text: "lion", image: "lion.jpg" },
+    m: { text: "mummy", image: "jen.jpg" },
     n: { text: "nana" },
-    o: { text: "owl" },
+    o: { text: "owl", image: "owl.jpg" },
     p: { text: "pape" },
-    q: { text: "queen" },
-    r: { text: "rat" },
-    s: { text: "sophie" },
-    t: { text: "tree" },
-    u: { text: "umbrella" },
-    v: { text: "van" },
-    w: { text: "water" },
-    x: { text: "xray" },
-    y: { text: "yellow" },
-    z: { text: "zebra" }
+    q: { text: "quilt", image: "quilt.jpg" },
+    r: { text: "rat", image: "rat.jpg" },
+    s: { text: "sophie", image: "sophie.jpg" },
+    t: { text: "tree", image: "tree.jpg" },
+    u: { text: "umbrella", image: "umbrella.jpg" },
+    v: { text: "van", image: "van.jpg" },
+    w: { text: "wolf", image: "wolf.jpg" },
+    x: { text: "xray", image: "xray.jpg" },
+    y: { text: "yogurt", image: "yogurt.jpg" },
+    z: { text: "zebra", image: "zebra.jpg" }
   };
 
   isLetter(str) {
@@ -46,6 +47,7 @@ export class AppComponent {
     if (this.isLetter(key)) {
       this.text = key.toUpperCase() + key.toLowerCase();
       this.name = this.letters[key] ? this.letters[key].text : "";
+      this.image = this.letters[key].image;
     } else {
       this.text = key;
     }
