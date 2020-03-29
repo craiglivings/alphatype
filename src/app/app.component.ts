8f411697c3544f517dc83bf54edff43a73273158
Craig Livings <CraigLivings@gmail.com>
2020-03-29 10:55:18 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'alphatype';
+  text = "Aa";
+  name = "amber";
+
+  letters = {
+    a: { text: "amber" },
+    b: { text: "banana" },
+    C: { text: "cat" },
+    d: { text: "daddy" },
+    e: { text: "elephant" },
+    f: { text: "house" },
+    g: { text: "giraffe" },
+    h: { text: "house" },
+    i: { text: "ice cream" },
+    j: { text: "house" },
+    k: { text: "house" },
+    l: { text: "lion" },
+    m: { text: "mummy" },
+    n: { text: "nana" },
+    o: { text: "owl" },
+    p: { text: "pape" },
+    q: { text: "queen" },
+    r: { text: "rat" },
+    s: { text: "sophie" },
+    t: { text: "tree" },
+    u: { text: "umbrella" },
+    v: { text: "van" },
+    w: { text: "water" },
+    x: { text: "xray" },
+    y: { text: "yellow" },
+    z: { text: "zebra" }
+  };
+
+  isLetter(str) {
+    return str.length === 1 && str.match(/[a-z]/i);
+  }
+
+  updateLetter(key) {
+    if (this.isLetter(key)) {
+      this.text = key.toUpperCase() + key.toLowerCase();
+      this.name = this.letters[key] ? this.letters[key].text : "";
+    } else {
+      this.text = key;
+    }
+  }
+
+  @HostListener("document:keypress", ["$event"])
+  handleKeyboardEvent = (event: KeyboardEvent) => {
+    const key = event.key;
+    this.updateLetter(key.toLowerCase());
+  };
 }
